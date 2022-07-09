e57d349a7e9aadaf08e1007269334ae75e7b0205
amn-max <ayushmnaik@gmail.com>
2022-07-09 17:16:03 +0530

    fd

--- hand_ges.py
+++ hand_ges.py
@@ -22,16 +22,21 @@ class VideoCap():
                 #perfrom sending data to server
                 cv2.imshow("Frame", img)
                 k = cv2.waitKey(1)
-                if hands:
-                    fingers = self.detector.fingersUp(hands[0])
-                    #code to sum of all 1's in fingers
-                    s = 0
-                    for i in fingers:
-                        if i == 1:
-                            s+=1
-                    await websocket.send(str(s))
-                    print(s)
-                await handler(websocket)
+                try:
+                    if hands:
+                        fingers = self.detector.fingersUp(hands[0])
+                        #code to sum of all 1's in fingers
+                        s = 0
+                        for i in fingers:
+                            if i == 1:
+                                s+=1
+                        await websocket.send(str(s))
+                        
+                        print(s)
+                except:
+                    print("No Hands")
+                
+                
                 if k == 1:
                     break
                 else:
@@ -63,6 +68,5 @@ if __name__ == "__main__":
     detector = HandDetector(detectionCon=0.8, maxHands=2)
     videoCap = VideoCap(cap,detector)
     videoCap.show_frame()
-    videoCap.destroy_frame()   
     
     
--- server.js
+++ server.js
@@ -11,6 +11,7 @@ wss.on("connection", function connection(ws) {
   ws.on("message", function message(data, isBinary) {
     wss.clients.forEach(function each(client) {
       if (client.readyState === WebSocket.OPEN) {
+        console.log(data);
         client.send(data, { binary: isBinary });
       }
     });
